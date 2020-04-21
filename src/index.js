5c4487c8f3014975dbeac233f8cbb2b59c71051a
lolapalmer <64013528+lolapalmer@users.noreply.github.com>
2020-04-21 20:45:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 
 exports.cToF = function (c) {
     const res = c*9/5 + 32;
-    return res.toFixed(2);
+    return res;
 }
 
 exports.fToC = function (f) {
     const res = (f-32) * 5/9
-    return res.toFixed(2);
+    return res;
